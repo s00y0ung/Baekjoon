c60f5b138461ff8ee6741bc530fa5b605a19013f
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2024-07-20 21:23:44 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+
+	vector<pair<int, int>> v(T);
+	for (int i = 0; i < T; i++)
+	{
+		cin >> v[i].first >> v[i].second;
+	}
+	for (int i = 0; i < T; i++)
+		cout << "Case #" << i + 1 << ": " << v[i].first << " + " << v[i].second << " = " <<
+		v[i].first + v[i].second << "\n";
+
+	return 0;
+}
