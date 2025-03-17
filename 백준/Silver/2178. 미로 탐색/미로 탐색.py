3bc8ee4a6a3c92f57e57d5649e0968123c9a791d
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-17 18:10:57 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 111540 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -3,37 +3,28 @@ import copy
 input = sys.stdin.readline
 
 def bfs():
-    global N, M, depth, map
-    visited = [[0 for i in range(M)] for i in range(N)]
+    global N, M, map, visited
     visited[0][0] = 1
     queue = [[0,0]]
-    depth = 2
-    q = []
 
     while queue:
         y,x = queue.pop(0)
-
         for d1,d2 in [[0,1],[0,-1],[1,0],[-1,0]]: #right, left, up, down
             ey = y + d1
             ex = x + d2
             if 0 <= ex < M and 0 <= ey < N:
                 if visited[ey][ex] == 0 and map[ey][ex] == 1:
-                    visited[ey][ex]=1
-                    q.append([ey,ex])
-
-        if visited[N-1][M-1] == 1:
+                    visited[ey][ex] = visited[y][x] + 1
+                    queue.append([ey,ex])
+        if visited[N-1][M-1] != 0:
             break
 
-        if len(queue) == 0:
-            queue = copy.deepcopy(q)
-            q = []
-            depth += 1
 
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
     map = [list(map(int, input().rstrip())) for i in range(N)]
-    depth = 0
+    visited = [[0 for i in range(M)] for i in range(N)]
 
     bfs()
-    print(depth)
+    print(visited[N-1][M-1])
