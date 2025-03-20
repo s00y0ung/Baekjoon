587e6d4205efa1028ec7d2e084c6b287d0928b97
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-20 14:27:50 +0900

    [Gold V] Title: 회의실 배정, Time: 256 ms, Memory: 55240 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,38 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def binarySearch(search, idx):
-    global meeting
-
-    left = 0
-    right = idx
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if meeting[mid][1] > search:
-            right = mid-1
-        elif meeting[mid][1] <= search:
-            left = mid + 1
-    return right
-
 if __name__ == "__main__":
     N = int(input())
 
     meeting = []
     for i in range(N):
         s, e = map(int, input().split())
-        meeting.append([s,e,1])
+        meeting.append([s,e])
     meeting.sort(key = lambda x : (x[1],x[0]))
 
+    cnt = 0
+    end = -1
+    for i in range(N):
+        if meeting[i][0] >= end:
+            end = meeting[i][1]
+            cnt += 1
 
-    for idx in range(1,N):
-        s,e,v = meeting[idx]
+    print(cnt)
 
-        exist = binarySearch(s, idx-1)
-        if exist == -1:
-            meeting[idx][2] = meeting[idx-1][2]
-        else:
-            meeting[idx][2] = max(1+meeting[exist][2], meeting[idx-1][2])
-
-    print(meeting[N-1][2])
