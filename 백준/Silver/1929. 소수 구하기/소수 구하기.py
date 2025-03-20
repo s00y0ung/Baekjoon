c1f1a299b9c8ad349fab847a452be06b55b630c7
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-20 15:13:45 +0900

    [Silver III] Title: 소수 구하기, Time: 288 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,16 +1,18 @@
-M,N = map(int, input().split())
+def solve():
+    M,N = map(int, input().split())
 
-prime_list = [i for i in range(N+1)]
-prime_list[1] = 0
-for i in range(1,N+1):
-    if prime_list[i] == 0:
-        continue
+    prime_list = [True] * (N+1)
+    prime_list[1] = False
 
-    k = 2
-    while i*k <= N:
-        prime_list[i*k] = 0
-        k += 1
+    for i in range(2,int(N**0.5)+1):
+        if prime_list[i]:
+            k = 2
+            while i*k <= N:
+                prime_list[i*k] = False
+                k += 1
 
-for i in range(M, N+1):
-    if prime_list[i] != 0:
-        print(prime_list[i])
+    for i in range(M, N+1):
+        if prime_list[i]:
+            print(i)
+
+solve()
