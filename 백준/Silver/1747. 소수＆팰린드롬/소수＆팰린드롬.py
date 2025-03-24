151189382d99819431ba5af6cd86427a8f381d5f
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-24 10:08:53 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 252 ms, Memory: 73428 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -5,18 +5,17 @@ def solve():
     prime[1] = 0
 
     # 소수 구하기
-    for i in range(2,len(prime)):
-
+    for i in range(2,int(len(prime) ** 0.5)):
         if prime[i] != 0:
             for j in range(i+i, len(prime), i):
                 prime[j] = 0
 
-            # 팰린드롬
-            if prime[i] >= N:
-                s = str(i)
-                if s == s[::-1]:
-                    print(s)
-                    break
-
+    # 팰린드롬 수
+    for i in range(N, len(prime)):
+        if prime[i] != 0:
+            s = str(i)
+            if s == s[::-1]:
+                print(s)
+                break
 
 solve()
