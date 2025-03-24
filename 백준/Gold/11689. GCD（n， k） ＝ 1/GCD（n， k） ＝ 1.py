8733026424f004bf906d9dc2f7b147f71c3b7776
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-24 15:32:39 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 112 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -1,23 +1,16 @@
 def solve():
     N = int(input())
-    prime = [i for i in range(int(N ** 0.5)+1)]
     ans = N
     p = []
 
     # 서로소 개수 구하기
     for i in range(2, int(N**0.5)+1):
-        if prime[i] == 0:
-            continue
-
         if ans % i == 0:
             p.append(i)
             while ans % i == 0:
                 ans = ans // i
             if ans == 1:
                 break
-
-        for k in range(i+i, int(N**0.5)+1,i):
-            prime[k]=0
 
     if ans != 1:
         p.append(ans)
