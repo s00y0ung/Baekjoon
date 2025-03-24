300d9fe4b3d00467ce89950b3efc111f29f2424a
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-24 16:03:39 +0900

    [Bronze I] Title: 최소공배수, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,11 @@
+def solve():
+    A, B = map(int, input().split())
+    m,n = A,B
+    while n != 0:
+        m,n = n, m%n
+    # 최대 공약수 == m
+    print(A*B//m)
+
+T = int(input())
+for i in range(T):
+    solve()
