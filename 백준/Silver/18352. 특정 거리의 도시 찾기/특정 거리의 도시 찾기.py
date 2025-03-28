985b5ac66742ed805f66849a5a859ff6aae45269
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-03-28 14:49:58 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1484 ms, Memory: 99760 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+
+def bfs(x):
+    global visited, graph, K
+    que = [x]
+    visited[x] = 0
+    depth = 0
+
+    while len(que) != 0:
+        for k in range(len(que)):
+            n = que.pop(0)
+            for i in range(len(graph[n])):
+                if visited[graph[n][i]] == -1:
+                    que.append(graph[n][i])
+                    visited[graph[n][i]] = visited[n]+1
+                    if visited[graph[n][i]] > depth:
+                        depth = visited[n]+1
+        if depth >= K:
+            break
+            
+    if K not in visited:
+        print(-1)
+    else:
+        for i in range(len(visited)):
+            if visited[i] == K:
+                print(i)
+    return 0
+
+N, M, K, X =map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [-1] * (N+1)
+
+for i in range(M):
+    A, B = map(int, input().split())
+    graph[A].append(B)
+
+bfs(X)
