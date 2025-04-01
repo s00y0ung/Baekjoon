ad547c5d753f39750cd38c039fcc6ad2dd7f6386
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-01 17:36:00 +0900

    [Gold V] Title: 집합의 표현, Time: 236 ms, Memory: 42708 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,37 +1,37 @@
 import sys
 input = sys.stdin.readline
 
+# union by rank
+#음수일 때 루트 크기 저장, 양수일 땐, 부모 인덱스 저장하는 방식임 ...
 def union(b,c):
     b_root = find(b)
     c_root = find(c)
 
-    if rank[b_root] > rank[c_root]:
+    if b_root == c_root: #이미 같은 집합인 경우
+        return
+
+    if parent[b_root] < parent[c_root]: #음수 비교, b의 집합 크기가 더 큰 경우
+        parent[b_root] += parent[c_root]
         parent[c_root] = b_root
-    elif rank[b_root] < rank[c_root]:
-        parent[b_root] = c_root
     else:
+        parent[c_root] += parent[b_root]
         parent[b_root] = c_root
-        rank[c_root] += 1
+
 
 def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
-
+    if parent[x] < 0:
+        return x
+    return find(parent[x])
 
 n, m = map(int, input().split())
-parent = [i for i in range(n+1)]
-rank = [0 for _ in range(n+1)]
+parent = [-1 for i in range(n+1)]
 
 for i in range(m):
     a,b,c  = map(int, input().split())
     if a == 0:
         union(b,c)
     elif a == 1:
-        b_parent = find(b)
-        c_parent = find(c)
-
-        if b_parent == c_parent:
+        if find(b) == find(c):
             print("YES")
         else:
             print("NO")
