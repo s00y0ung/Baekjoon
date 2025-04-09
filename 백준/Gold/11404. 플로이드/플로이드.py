52b1f9f737e89e1f725bc3503f7f77a8366637f8
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-09 14:29:55 +0900

    [Gold IV] Title: 플로이드, Time: 376 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -4,17 +4,20 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 
-graph =[[1e9 for _ in range(N+1)] for _ in range(N+1)]
+graph =[[1e9] * (N+1) for _ in range(N+1)]
 for i in range(N+1):
     graph[i][i] = 0
 for _ in range(M):
     a,b,c = map(int, input().split())
-    graph[a][b] = min(c, graph[a][b])
+    if graph[a][b] > c:
+        graph[a][b] = c
 
 for k in range(1,N+1):
     for i in range(1, N+1):
-        for j in range(1, N+1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+        if graph[i][k] != 1e9:
+            for j in range(1, N+1):
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
 
 for idx in range(1,N+1):
     for g in graph[idx][1:]:
