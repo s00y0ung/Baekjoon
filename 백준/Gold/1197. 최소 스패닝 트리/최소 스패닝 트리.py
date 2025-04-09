e4c88ad2cf88a14354ae60d4de23056e14f5a34d
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-09 17:17:20 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 300 ms, Memory: 57288 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    while parent[x] != x:
+        x = parent[x]
+    return x
+
+def unionByRank(rank, a, b):
+    a = find(a)
+    b = find(b)
+
+    if a == b:
+        return
+
+    if rank[a] > rank[b]:
+        parent[b] = a
+    elif rank[a] < rank[b]:
+        parent[a] = b
+    else:
+        rank[b] += 1
+        parent[a] = b
+
+V, E = map(int, input().split())
+edges = []
+parent = [i for i in range(V+1)]
+rank = [0] * (V+1)
+for _ in range(E):
+    edges.append(list(map(int, input().split())))
+edges.sort(key = lambda x : x[2])
+
+result = 0
+for e in edges:
+    u,v,w = e
+    if find(u) == find(v):
+        continue
+    unionByRank(rank,u,v)
+    result += w
+print(result)
