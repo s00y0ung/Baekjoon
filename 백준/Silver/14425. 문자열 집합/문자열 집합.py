dca84cb37a30ff450096729da8f8f6145f5cbd1a
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-10 16:48:36 +0900

    [Silver IV] Title: 문자열 집합, Time: 88 ms, Memory: 37444 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,33 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-def binarySearch(s,f):
-    left = 0
-    right = len(N_list[f])-1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if s > N_list[f][mid]:
-            left = mid + 1
-        elif s < N_list[f][mid]:
-            right = mid -1
-        else:
-            return True
-    return False
-
 N, M = map(int, input().split())
-N_list = [[] for _ in range(26)]
-
+N_list = set()
 for _ in range(N):
-    n_str = input().rstrip()
-    N_list[ord(n_str[0])-97].append(n_str)
-for i in range(26):
-    N_list[i].sort()
+    N_list.add(input().rstrip())
+
 cnt = 0
 for _ in range(M):
     m_str = input().rstrip()
-    m_first = ord(m_str[0])-97
-    if binarySearch(m_str, m_first):
+    if m_str in N_list:
         cnt += 1
 
 print(cnt)
