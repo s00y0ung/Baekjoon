374b3033b0f87abd9fd771b2e8d851dd56434103
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-11 21:40:16 +0900

    [Gold IV] Title: 타임머신, Time: 292 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -5,25 +5,26 @@ def bf(start):
     distance[start] = 0
 
     for i in range(N):
-        for j in range(M):
-            cur = graph[j][0]
-            next = graph[j][1]
-            weight = graph[j][2]
+        for j in range(1, N+1):
 
-            if distance[cur] != 1e9 and distance[next] > distance[cur] + weight:
-                distance[next] = distance[cur] + weight
-                if i == N-1:
-                    return True
+            if distance[j] == 1e9:
+                continue
+
+            for u,w in graph[j]:
+                if distance[u] > distance[j] + w:
+                    distance[u] = distance[j]+w
+                    if i == N-1:
+                        return True
     return False
 
 
 N, M = map(int,input().split())
-graph =[]
+graph =[[] for _ in range(N+1)]
 distance = [1e9 for _ in range(N+1)]
 
 for _ in range(M):
     s,e,w = map(int, input().split())
-    graph.append((s,e,w))
+    graph[s].append((e,w))
 
 if bf(1):
     print(-1)
