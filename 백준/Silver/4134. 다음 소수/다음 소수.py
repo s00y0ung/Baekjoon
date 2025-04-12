5e90c349fd2a73527d634b2f9b6fc164f9bde652
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-12 22:05:15 +0900

    [Silver IV] Title: 다음 소수, Time: 1668 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -8,17 +8,19 @@ def check(x):
     return True
 
 N = int(input())
-prime = [0 for _ in range(4 * 1000000 + 1)]
 
 for _ in range(N):
     k = int(input())
+    if k == 1 or k == 0 or k == 2:
+        print(2)
+        continue
+    
+    if k % 2 == 0:
+        k += 1
+
     while True:
-        if k == 1 or k == 0:
-            print(2)
-            break
-        
         if check(k):
             print(k)
             break
         else:
-            k += 1
+            k += 2
