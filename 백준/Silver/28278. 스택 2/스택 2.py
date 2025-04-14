ced4465fd802e71273ab2763b360bdefefbf4f4c
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-14 20:14:05 +0900

    [Silver IV] Title: 스택 2, Time: 956 ms, Memory: 98900 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -4,24 +4,19 @@ input = sys.stdin.readline
 N = int(input())
 
 stack = []
+output = []
+
 for _ in range(N):
     i_list = list(map(int, input().split()))
     if i_list[0] == 1:
         stack.append(i_list[1])
     elif i_list[0] == 2:
-        if not stack:
-            print(-1)
-        else:
-            print(stack.pop(len(stack)-1))
+        output.append(-1 if not stack else stack.pop())
     elif i_list[0] == 3:
-        print(len(stack))
+        output.append(len(stack))
     elif i_list[0] == 4:
-        if not stack:
-            print(1)
-        else:
-            print(0)
+        output.append(1 if not stack else 0)
     elif i_list[0] == 5:
-        if not stack:
-            print(-1)
-        else:
-            print(stack[len(stack)-1])
+        output.append(-1 if not stack else stack[-1])
+
+print('\n'.join(map(str,output)))
