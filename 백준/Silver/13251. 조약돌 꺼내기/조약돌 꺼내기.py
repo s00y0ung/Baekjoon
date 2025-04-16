d0c9a9eb3b5b533285bf88e7931579da57d093be
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-16 16:13:41 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
@@ -1,14 +1,7 @@
 import sys
+import math
 input = sys.stdin.readline
 
-def factorial(a):
-    ans = 1
-    for idx in range(1, a+1):
-        ans = ans * idx
-    return ans
-
-def get_c(n,k):
-    return factorial(n) // (factorial(k) * factorial(n-k))
 
 M = int(input()) # 조약돌 색상
 N_list = list(map(int, input().split())) # 조약돌 개수
@@ -17,5 +10,5 @@ K = int(input()) # K 개수
 total = sum(N_list)
 t = 0
 for n in N_list:
-    t += factorial(n) // (factorial(K) * factorial(n-K))
-print(t / (factorial(total) // (factorial(K) * factorial(total-K)))) 
+    t += math.comb(n, K)
+print(t / math.comb(total, K))
