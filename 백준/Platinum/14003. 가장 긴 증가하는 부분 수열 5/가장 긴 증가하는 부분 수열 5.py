5c489b9919175c3dc367abb87c1616bdfde89c52
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-24 21:38:10 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 2580 ms, Memory: 160152 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,47 +1,39 @@
-n=int(input())
-arr = list(map(int,input().split()))
+import sys
+input = sys.stdin.readline
 
-LIS = [arr[0]]
-# dp에는 LIS에 들어간 인덱스를 저장해둔다.
-dp = [(0,arr[0])] 
+def binary_search(a, lis):
+    left = 0
+    right = len(lis)
 
-def binarySearch(e):
-    start = 0
-    end = len(LIS) - 1
-    
-    while start <= end:
-        mid = (start + end) // 2
-        
-        if LIS[mid] == e:
-            return mid
-        elif LIS[mid] < e:
-            start = mid + 1
+    while left < right:
+        mid = (left + right)//2
+        if lis[mid] < a:
+            left = mid + 1
         else:
-            end = mid - 1
-            
-    return start
+            right = mid
+    return right
 
-
-for i in range(1,n):
-    if arr[i] > LIS[-1]:
-        LIS.append(arr[i])
-        dp.append((len(LIS)-1, arr[i]))
-
+N = int(input())
+a_list = list(map(int, input().split()))
+lis = [a_list[0]]
+ans = [0 for i in range(N)]
+idx = 0
+for a in a_list[1:]:
+    idx += 1
+    if a > lis[-1]:
+        lis.append(a)
+        ans[idx] = len(lis)-1
     else:
-        idx = binarySearch(arr[i])
-        LIS[idx] = arr[i]
-        dp.append((idx, arr[i]))
+        b = binary_search(a, lis)
+        lis[b] = a
+        ans[idx] = b
 
+s = len(lis) - 1
+s_list = []
+for idx in range(N-1,-1,-1):
+    if ans[idx] == s:
+        s_list.append(a_list[idx])
+        s -= 1
 
-print(len(LIS))
-
-# 역추적
-last_idx = len(LIS) - 1
-res = []
-for i in range(len(dp)-1, -1, -1):
-    # i번째 값의 index와 마지막 인덱스값과 같다면
-    if dp[i][0] == last_idx:
-        res.append(dp[i][1])
-        last_idx-=1
-
-print(*res[::-1])
+print(len(lis))
+print(*s_list[::-1])
