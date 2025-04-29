5516792069647d27e5b7e02446b5bb770a3135d3
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-29 17:01:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,48 +1,50 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10000000)
-def backTracking(depth):
-    if depth == N-1:
+def backTracking(s, depth):
+    if depth == N:
         global minN, maxN
 
-        ans = n_list[0]
-        for i in range(1, N):
-            if s[i-1] == 0:
-                ans += n_list[i]
-            elif s[i-1] == 1:
-                ans -= n_list[i]
-            elif s[i-1] == 2:
-                ans *= n_list[i]
-            elif s[i-1] == 3:
-                if ans * n_list[i] < 0:
-                    ans = abs(ans) // abs(n_list[i])
-                    ans = ans*-1
-                else:
-                    ans = ans // n_list[i]
-                    
-        if maxN < ans:
-            maxN = ans
-        if minN > ans:
-            minN = ans
+        if maxN < s:
+            maxN = s
+        if minN > s:
+            minN = s
         return
 
+    if ex_list[0] != 0:
+        ex_list[0] -= 1
+        backTracking(s+n_list[depth], depth+1)
+        ex_list[0] += 1
 
-    for i in range(4):
-        if ex_list[i] != 0:
-            s.append(i)
-            ex_list[i] -= 1
-            backTracking(depth+1)
-            s.pop(-1)
-            ex_list[i] += 1
+    if ex_list[1] != 0:
+        ex_list[1] -= 1
+        backTracking(s-n_list[depth], depth+1)
+        ex_list[1] += 1
+
+    if ex_list[2] != 0:
+        ex_list[2] -= 1
+        backTracking(s*n_list[depth], depth+1)
+        ex_list[2] += 1
+
+    if ex_list[3] != 0:
+        ex_list[3] -= 1
+        tmp = s // n_list[depth]
+        if s * n_list[depth] < 0:
+            tmp = abs(s) // abs(n_list[depth])
+            tmp *= -1
+        backTracking(tmp, depth+1)
+        ex_list[3] += 1
 
 
 N = int(input())
 n_list = list(map(int, input().split()))
-s = []
 ex_list = list(map(int, input().split()))
 
 minN = 100000000000
 maxN = -100000000000
-backTracking(0)
+
+s = n_list[0]
+backTracking(s,1)
+
 print(maxN)
 print(minN)
