e2182365ae8cbc38946a0329b0ba66183da1ea14
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-30 21:41:36 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,30 +1,23 @@
-dp = [[[0 for _ in range(21)] for _ in range(21)]for _ in range(21)]
+import sys
+input = sys.stdin.readline
 
-def dpTable():
-        for a in range(21):
-            for b in range(21):
-                for c in range(21):
-                    if a <= 0 or b <= 0 or c <= 0:
-                        dp[a][b][c] = 1
-                    elif a < b and b < c:
-                        dp[a][b][c] = dp[a][b][c-1] + dp[a][b-1][c-1] - dp[a][b-1][c]
-                    else:
-                        dp[a][b][c] = (
-                        dp[a-1][b][c] +
-                        dp[a-1][b-1][c] +
-                        dp[a-1][b][c-1] -
-                        dp[a-1][b-1][c-1]
-                    )
-        return dp
-dp = dpTable()
+dp = [[[-1 for _ in range(21)] for _ in range(21)] for _ in range(21)]
+for i in range(21):
+    for j in range(21):
+        for k in range(21):
+            if i == 0 or j == 0 or k == 0:
+                dp[i][j][k] = 1
+            elif i < j < k:
+                dp[i][j][k] = dp[i][j][k-1] + dp[i][j-1][k-1] - dp[i][j-1][k]
+            else:
+                dp[i][j][k] = dp[i-1][j][k] + dp[i-1][j-1][k] + dp[i-1][j][k-1] - dp[i-1][j-1][k-1]
 
-while True:
-    a,b,c = map(int, input().split())
-    if a == -1 and b == -1 and c == -1:
-        break
+a,b,c = map(int, input().split())
+while a != -1 or b != -1 or c != -1:
     if a <= 0 or b <= 0 or c <= 0:
-        print(f"w({a}, {b}, {c}) = 1")
+        print(f'w({a}, {b}, {c}) = 1')
     elif a > 20 or b > 20 or c > 20:
-        print(f"w({a}, {b}, {c}) = {dp[20][20][20]}")
+        print(f'w({a}, {b}, {c}) = {dp[20][20][20]}')
     else:
-        print(f"w({a}, {b}, {c}) = {dp[a][b][c]}")
+        print(f'w({a}, {b}, {c}) = {dp[a][b][c]}')
+    a, b, c = map(int, input().split())
