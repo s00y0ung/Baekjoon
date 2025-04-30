730fb95eea0be8be749b28339de7dde3e7e50f00
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-30 15:49:29 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 41760 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,31 +1,16 @@
-import sys
-input = sys.stdin.readline
-
-def star(N,i,j):
-    if N == 3:
-        star_list[i][j] = '*'
-        star_list[i][j+1] = '*'
-        star_list[i][j+2] = '*'
-        star_list[i+1][j] = '*'
-        star_list[i+1][j+2] = '*'
-        star_list[i+2][j] = '*'
-        star_list[i+2][j+1] = '*'
-        star_list[i+2][j+2] = '*'
-        return
-    star(N // 3, i+0,j+0)
-    star(N // 3, i+0, j+N//3)
-    star(N // 3, i+0, j+N//3*2)
-
-    star(N // 3, i+N//3, j+0)
-    star(N // 3, i+N//3, j+N//3*2)
-
-    star(N // 3, i+N//3*2,j+0)
-    star(N // 3, i+N//3*2, j+N // 3)
-    star(N // 3, i+N//3*2, j+N // 3 * 2)
+def star(n) :
+    if n == 3 :
+        return  ["***","* *","***"]
+    div = n//3
+    res = []
+    l = star(div)
+    for i in l :
+        res.append(i*3)
+    for i in l :
+        res.append(i+' '*(div)+i)
+    for i in l :
+        res.append(i*3)
+    return res
 
 N = int(input())
-star_list = [[' ' for _ in range(N)] for _ in range(N)]
-
-star(N,0,0)
-for i in range(N):
-    print(''.join(star_list[i]))
+print("\n".join(star(N)))
