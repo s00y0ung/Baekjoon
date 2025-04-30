2c1ae83d19a667defbd73abf0d2ecef33e9fbc99
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-04-30 17:25:48 +0900

    [Silver III] Title: 01타일, Time: 248 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,6 +1,9 @@
 N = int(input())
-dp = [1 for i in range(N+1)]
-dp[1] = 1
+p = 1
+pp = 1
+cur = 1
 for idx in range(2,N+1):
-    dp[idx] = (dp[idx-2] + dp[idx-1]) % 15746
-print(dp[N]%15746)
+    cur = (p+pp)%15746
+    pp = p
+    p = cur
+print(cur%15746)
