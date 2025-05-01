260cfc30bcffba26ee8bf3224f6d30e7de8f25ec
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-05-01 15:29:15 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,12 +1,11 @@
+import sys
+input = sys.stdin.readline 
 N = int(input())
 
-dp = [1 for _ in range(101)]
-dp[0] = 0
-dp[4] = 2
-dp[5] = 2
+dp = [0,1,1,1,2,2]
 
 for i in range(6,101):
-    dp[i] = dp[i-5] + dp[i-1]
+    dp.append(dp[i-5] + dp[i-1])
 
 for _ in range(N):
     t = int(input())
