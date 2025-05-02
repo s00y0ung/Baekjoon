7117fbf9572ea7304097c6cbd12f25f50ef172c9
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-05-02 20:39:45 +0900

    [Silver I] Title: 정수 삼각형, Time: 104 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -5,11 +5,11 @@ n = int(input())
 graph = []
 
 for i in range(n):
-    graph.append([-1] + list(map(int, input().split())))
-dp = [[0 for _ in range(n+1)] for _ in range(n)]
-dp[0][1] = graph[0][1]
+    graph.append([-1] + list(map(int, input().split())) + [-1])
+#dp = [[0 for _ in range(n+1)] for _ in range(n)]
+#dp[0][1] = graph[0][1]
 
 for h in range(1,n):
     for j in range(1,h+2):
-        dp[h][j] = max(dp[h-1][j-1], dp[h-1][j]) + graph[h][j]
-print(max(dp[n-1]))
+        graph[h][j] = max(graph[h-1][j-1], graph[h-1][j]) + graph[h][j]
+print(max(graph[n-1]))
