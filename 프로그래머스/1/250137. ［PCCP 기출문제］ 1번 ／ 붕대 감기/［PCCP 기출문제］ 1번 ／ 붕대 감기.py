19c52de9f409ec4ace78d4f40a2ac75fbc403f45
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-05-16 20:33:35 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,22 +1,15 @@
 def solution(bandage, health, attacks):
     answer = health
-    at = 0
-    bt = 0
-    for t in range(attacks[-1][0] + 1):
-        if t == attacks[at][0]:
-            answer -= attacks[at][1]
-            at += 1
-            bt = 0
-            if answer <= 0:
-                return -1
-        else:
-            bt += 1
-            answer += bandage[1]
-            if answer >= health:
-                answer = health
-                continue
-            if bt == bandage[0]:
-                bt = 0
-                answer += bandage[2]  
+    attacks.append([0])
+    for t in range(len(attacks)-1):
+        band_time = attacks[t][0] - attacks[t-1][0] - 1
+        bt = band_time // bandage[0]
+        answer = answer + bt * bandage[2] + band_time * bandage[1]
+        if answer > health:
+            answer = health
+
+        answer -= attacks[t][1]
+        if answer <= 0:
+            return -1
         
     return answer
