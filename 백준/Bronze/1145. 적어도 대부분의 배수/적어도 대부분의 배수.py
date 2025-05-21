0e5d2c8c6afba4d8ff6350022d3ff56d70666b6c
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-05-21 15:35:52 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -1,14 +1,16 @@
-list = list(map(int, input().split()))
+def gcd(a,b):
+    while b > 0:
+        a,b = b, a%b
+    return a
 
-min_num = 1
-while True:
-    min_num += 1
-    a = 1 if min_num % list[0] == 0 else 0
-    b = 1 if min_num % list[1] == 0 else 0
-    c = 1 if min_num % list[2] == 0 else 0
-    d = 1 if min_num % list[3] == 0 else 0
-    e = 1 if min_num % list[4] == 0 else 0
+def lcm(a, b):
+    return a*b // gcd(a,b)
 
-    if a+b+c+d+e >= 3:
-        print(min_num)
-        break
+nums = list(map(int, input().split()))
+answer = 1000000
+
+for i in range(3):
+    for j in range(i+1, 5):
+        for k in range(j+1, 5):
+            answer = min(answer, lcm(lcm(nums[i], nums[j]), nums[k]))
+print(answer)
