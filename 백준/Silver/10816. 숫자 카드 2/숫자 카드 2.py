e4f876187a2e56c0b528bfb378656c25c4c030e3
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-05-26 12:29:40 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 708 ms, Memory: 115772 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,19 @@
+N = int(input())
+cards = list(map(int, input().split()))
+M = int(input())
+finds = list(map(int, input().split()))
+
+cards_dic = {}
+for c in cards:
+    if c in cards_dic:
+        cards_dic[c] += 1
+    else:
+        cards_dic[c] = 1
+
+ans = []
+for f in finds:
+    if f in cards_dic:
+        ans.append(cards_dic[f])
+    else:
+        ans.append(0)
+print(*ans)
