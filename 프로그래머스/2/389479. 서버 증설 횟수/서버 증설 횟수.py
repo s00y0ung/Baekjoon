144e4287eac28218754b275f43a976d7bf21a058
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-04 15:40:01 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -0,0 +1,19 @@
+def solution(players, m, k):
+    answer = 0
+    cnt = [0 for i in range(24)]
+    cnt[0] = players[0] // m
+    if cnt[0] != 0:
+        answer += cnt[0]
+        cnt[k] = -cnt[0]
+    
+    for i in range(1,24):
+        cnt[i] = cnt[i-1]+cnt[i]
+        #서버 증설이 필요한 경우
+        if players[i] >= (cnt[i]+1)*m:
+            c = (players[i]//m) - cnt[i]
+            answer += c
+            cnt[i] += c
+            if i+k < 24:
+                cnt[i+k] -= c
+        
+    return answer
