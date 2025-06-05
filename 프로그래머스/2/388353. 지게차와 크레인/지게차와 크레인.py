39dd9bbefde775a47c1181c962453e9522e45a10
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-05 15:59:42 +0900

    [level 2] Title: 지게차와 크레인, Time: 2598.14 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -0,0 +1,51 @@
+def bfs(s, i, j, N, M):
+    if i == N - 1 or j == M-1 or i == 0 or j == 0:
+        return 1
+
+    que = [(i,j)]
+    visited = [(i,j)]
+    while que:
+        i = que[0][0]
+        j = que[0][1]
+        del que[0]
+        
+        if i == N - 1 or j == M - 1 or i == 0 or j == 0:
+            return 1
+        for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            if s[i + x][j + y] == '-' and (i+x,j+y) not in visited:
+                que.append((i+x,j+y))
+                visited.append((i+x,y+j))
+    return -1
+
+def solution(storage, requests):
+    N = len(storage)
+    M = len(storage[0])
+    answer = N * M
+
+    s = []
+    for i in range(len(storage)):
+        s.append(list(storage[i]))
+
+    for request in requests:
+        if len(request) == 2:
+            for i in range(N):
+                for j in range(M):
+                    if s[i][j] == request[0]:
+                        answer -= 1
+                        s[i][j] = '-'
+        else:
+            remove = []
+            for i in range(N):
+                for j in range(M):
+                    if s[i][j] != request:
+                        continue
+
+                    if bfs(s, i, j, N, M) == 1:
+                        remove.append((i, j))
+
+            for rx, ry in remove:
+                s[rx][ry] = '-'
+                answer -= 1
+
+    return answer
+
