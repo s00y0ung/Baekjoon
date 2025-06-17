cd32b17a2b10e05659fb9f8d46ed9a24356703c1
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-17 11:29:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 1369.29 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,28 +1,12 @@
 def solution(id_list, report, k):
-    answer = [0 for i in range(len(id_list))]
-    id_dic = {}
-    stop = {}
-    for i in id_list:
-        id_dic[i] = set()
-        stop[i] = 0
-        
-    for r in report:
-        a,b = r.split(' ')
-        if b in id_dic[a]:
-            continue
-        id_dic[a].add(b)
-        stop[b] += 1
+    answer = [0] * len(id_list)
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
     
-    stop_list = []
-    for i in id_list:
-        if stop[i] >= k:
-            stop_list.append(i)
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
     
-    cnt = 0
-    for k in id_list:
-        for s in stop_list:
-            if s in id_dic[k]:
-                answer[cnt] += 1
-        cnt += 1
-   
     return answer
