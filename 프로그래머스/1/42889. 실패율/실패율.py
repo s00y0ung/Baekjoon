9c2fa2450783aad531ac173bed49f145590aa5c7
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-20 12:31:51 +0900

    [level 1] Title: 실패율, Time: 23.90 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,22 +1,16 @@
-
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
 def solution(N, stages):
-    result = {}
-    denominator = len(stages)
-    for stage in range(1, N+1):
-        if denominator != 0:
-            count = stages.count(stage)
-            result[stage] = count / denominator
-            denominator -= count
+    answer = {}
+    challenge = [0 for i in range(N + 1)]
+    for s in stages:
+        challenge[s - 1] += 1
+
+    total = len(stages)
+    for c in range(N):
+        if total == 0:
+            answer[c+1] = 0
         else:
-            result[stage] = 0
-    return sorted(result, key=lambda x : result[x], reverse=True)
+            answer[c+1] = challenge[c] / total
+            total -= challenge[c]
+
+    answer = sorted(answer, key=lambda x: answer[x], reverse = True)
+    return answer
