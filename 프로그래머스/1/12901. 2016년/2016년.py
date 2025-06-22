40fbab8104a5ffa8b20ce80f3dcaf2ea0bc9d17c
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-22 22:01:55 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,10 +1,9 @@
 def solution(a, b):
-    month = [0,31,29,31,30,31,30,31,31,30,31,30,31]
-    dayofweek = ['FRI','SAT','SUN','MON','TUE','WED','THU']
-    
-    day = -1
-    for i in range(1,a):
-        day += month[i]
-    day += b
+    month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    dayofweek = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
 
-    return dayofweek[day%7]
+    day = b-1
+    for i in range(1, a):
+        day += month[i]
+
+    return dayofweek[day % 7]
