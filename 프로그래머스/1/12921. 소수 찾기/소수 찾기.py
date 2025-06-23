e46c609950bf2fe9f3d73f4241840ed5c92a15e2
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-23 12:42:42 +0900

    [level 1] Title: 소수 찾기, Time: 355.07 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,11 +1,7 @@
 def solution(n):
-    answer = [1 for i in range(n+1)]
-    answer[0] = 0
-    answer[1] = 0
+    num = set(range(2, n+1))
     for i in range(2, n+1):
-        if answer[i] == 0:
-            continue
-        for j in range(i+i, n+1, i):
-            answer[j] = 0
-        
-    return sum(answer)
+        if i in num:
+            num -= set(range(i+i, n+1,i))
+
+    return len(num)
