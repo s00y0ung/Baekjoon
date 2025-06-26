f19794ca7e2adf8f34eaf8352a0150a31c2a773e
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-26 21:05:24 +0900

    [Silver II] Title: 숫자 야구, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,44 +1,31 @@
-def base_game(abc_list, abc):
-    a1,b1,c1 = abc_list[0], abc_list[1], abc_list[2]
-    a2,b2,c2 = abc[0], abc[1], abc[2]
+n = int(input())
+base = []
+for i in range(n):
+    base.append(list(map(int, input().split())))
 
-    strike, ball = 0,0
-
-    if a1 == a2: strike += 1
-    elif a1 in abc : ball += 1
-
-    if b1 == b2: strike += 1
-    elif b1 in abc : ball += 1
-
-    if c1 == c2: strike += 1
-    elif c1 in abc : ball += 1
-
-    return strike, ball
-
-N = int(input())
-
-abc_list = []
+answer = 0
 for i in range(123, 988):
-    abc = str(i)
-    if '0' in abc: 
+    if i%10 == 0 or i//10%10==0:
         continue
-    if abc[0] == abc[1] or abc[1] == abc[2] or abc[0] == abc[2]:
-        continue        
-    abc_list.append(str(i))
+    if i%10 == i//100 or i%10 == i//10%10 or i//100 == i//10%10:
+        continue
 
+    num = str(i)
+    f = 1
+    for b, strike, ball in base:
+        b = str(b)
+        s_cnt = 0
+        b_cnt = 0
+        if num[0] == b[0]: s_cnt += 1
+        elif num[0] in b: b_cnt += 1
+        if num[1] == b[1]: s_cnt += 1
+        elif num[1] in b: b_cnt += 1
+        if num[2] == b[2]: s_cnt += 1
+        elif num[2] in b: b_cnt += 1
 
-for i in range(N):
-    abc, strike, ball = map(int, input().split())
-
-    abc_index = 0
-    while abc_index < len(abc_list):
-        s,b = base_game(abc_list[abc_index], str(abc))
-        if strike != s or ball != b: # 생존이 아닌 경우
-            del abc_list[abc_index]
-            abc_index -= 1
-        abc_index += 1
-
-print(len(abc_list))
-        
-    
-    
+        if strike != s_cnt or ball != b_cnt:
+            f = 0
+            break
+    if f:
+        answer += 1
+print(answer)
