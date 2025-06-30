97b68e1bde9c5c159834a1b5f8ae9df33d81346c
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-06-30 12:06:33 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = 1
-    cnt = str(format(n, 'b')).count('1')
-    while str(format(n+answer,'b')).count('1') != cnt:
+    cnt = bin(n).count('1')
+    while bin(n+answer).count('1') != cnt:
         answer += 1
     return answer+n
