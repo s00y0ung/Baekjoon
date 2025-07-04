bccb0a3996187c49424dada9d040e049dbea9802
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-04 16:10:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.10 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,12 +1,9 @@
-def get_gcd(a,b):
-    while b != 0:
-        a, b = b, a%b
-    return a
+from fractions import gcd
 
 def solution(arr):
     answer = arr[0]
     for a in arr[1:]:
-        tmp = get_gcd(a, answer)
+        tmp = gcd(a, answer)
         answer = a*answer//tmp
         
     return answer
