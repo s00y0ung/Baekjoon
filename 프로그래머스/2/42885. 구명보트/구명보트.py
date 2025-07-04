ade1d7334edebdd25b58bfc57a12ac4b1ba15030
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-04 16:04:02 +0900

    [level 2] Title: 구명보트, Time: 9.45 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -4,17 +4,11 @@ def solution(people, limit):
     si, ei = 0, len(people)-1
     
     while si <= ei:
-        s = people[ei]
-        ei -= 1
-        if s + people[ei] <= limit:
-            s += people[ei]
-            ei -= 1
-            answer += 1
-            continue
-
-        if s + people[si] <= limit:
-            s += people[si]
+        if people[si] + people[ei] <= limit:
             si += 1
+            ei -= 1
+        else:
+            ei -= 1
         answer += 1
             
     return answer
