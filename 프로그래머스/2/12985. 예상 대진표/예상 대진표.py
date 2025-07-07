4008eff650b38750609fe55a5c97dc1ad0d5ab09
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-07 17:14:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -2,12 +2,8 @@ def solution(n,a,b):
     answer = 0
     
     while b != a:
-        if b % 2 == 1:
-            b += 1
-        if a % 2 == 1:
-            a += 1
-        a = a // 2
-        b = b // 2
+        a = (a+1) // 2
+        b = (b+1) // 2
         answer += 1
 
     return answer
