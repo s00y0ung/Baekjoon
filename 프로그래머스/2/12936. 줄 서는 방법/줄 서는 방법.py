a4756c78f3b959a3fe85798ef27a611c083172c7
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-11 17:43:21 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 9.01 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,18 +1,17 @@
+from math import factorial
+
 def solution(n,k):
     answer = []
     n_list = [i for i in range(1, n+1)]
     k = k-1
-    while n > 1:
-        f = 1
-        for i in range(1, n):
-            f = f * i
-        a = k // f
 
-        answer.append(n_list[a])
-        n_list.pop(a)
-        k = k%(f)
+    while n > 0:
+        f = factorial(n-1)
+
+        answer.append(n_list[k // f])
+        n_list.pop(k // f)
+        k = k % f
+
         n = n-1
-        
-    answer.append(n_list[0])
 
     return answer
