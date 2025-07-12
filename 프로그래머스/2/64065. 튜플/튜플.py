d69062b8915e88c38bb2f419636bb5ebf7e23015
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-12 22:21:54 +0900

    [level 2] Title: 튜플, Time: 325.60 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,12 +1,27 @@
-from collections import Counter
 def solution(s):
-    s = s.replace('{',"")
-    s = s.replace('}',"")
-    s = s.split(',')
-    
-    t = Counter(s)
-    t = sorted(t.items(), key = lambda x : -x[1])
     answer = []
-    for a in t:
-        answer.append(int(a[0]))
+
+    s1 = s.lstrip('{').rstrip('}').split('},{')
+
+    new_s = []
+    for i in s1:
+        new_s.append(i.split(','))
+
+    new_s.sort(key = len)
+
+    for i in new_s:
+        for j in range(len(i)):
+            if int(i[j]) not in answer:
+                answer.append(int(i[j]))
+
     return answer
+# from collections import Counter
+# def solution(s):
+#     s = s.split('{,}')
+#     print(s)
+#     t = Counter(s)
+#     t = sorted(t.items(), key = lambda x : -x[1])
+#     answer = []
+#     for a in t:
+#         answer.append(int(a[0]))
+#     return answer
