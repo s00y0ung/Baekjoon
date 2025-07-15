0e62e524c3fafb9402dcb2733926faddb68bca5c
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-15 23:35:35 +0900

    [level 2] Title: 소수 찾기, Time: 1030.59 ms, Memory: 51.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+def solution(numbers):
+    answer = 0
+    n_set = set()
+    for n in range(1, len(numbers)+1):
+        for c in permutations(numbers, n):
+            num = int(''.join(c))
+            n_set.add(num)
+            
+    prime = [0 for _ in range(max(n_set)+1)]
+    prime[0] = 1
+    prime[1] = 1
+    for i in range(2,int(len(prime)**0.5)+1):
+        if prime[i] == 1:
+            continue
+        for j in range(i+i, len(prime), i):
+            prime[j] = 1
+    
+    for s in n_set:
+        if prime[s] == 0:
+            answer += 1
+            
+    return answer
