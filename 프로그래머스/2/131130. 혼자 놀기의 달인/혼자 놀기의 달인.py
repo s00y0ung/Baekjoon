ca3c033f7b5aa36e72cad7a5fff6443536cd33cd
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-18 14:39:59 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,19 +1,20 @@
 def solution(cards):
     answer = []
     cards = [0] + cards
-    visited = [0 for i in range(len(cards))]
     
-    for c in cards[1:]:
-        if visited[c] == 1:
+    for c in range(1, len(cards)):
+        if cards[c] == -1:
             continue
         cnt = 0
-        while visited[c] == 0:
-            visited[c] = 1
-            c = cards[c]
+        while cards[c] != -1:
+            tmp = cards[c]
+            cards[c] = -1
+            c = tmp
             cnt += 1
         answer.append(cnt)
         
     if len(answer) == 1:
         return 0
     answer.sort()
+
     return answer[-1]*answer[-2]
