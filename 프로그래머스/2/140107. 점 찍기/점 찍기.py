db50541c307c59e39620cc62f6cbb859eb31eb58
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-18 12:24:13 +0900

    [level 2] Title: 점 찍기, Time: 678.97 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -1,6 +1,7 @@
+
 def solution(k, d):
-    answer = 0
-    for i in range(0,d+1,k):
-        answer += int((d*d-i*i)**0.5)//k+1
-        
-    return answer
+    c = 0
+    for y in range(0, d, k):
+        x = (d**2 - y**2)**0.5
+        c += x//k
+    return c + d//k + 1
