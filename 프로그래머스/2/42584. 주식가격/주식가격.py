c75488d7a583d209675f274ade3c1faf90077338
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-07-24 14:24:30 +0900

    [level 2] Title: 주식가격, Time: 34.19 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,13 @@
 def solution(prices):
-    answer = [0 for _ in range(len(prices))]
+    stack = []
+    answer = [0]*len(prices)
 
-    for p in range(len(prices)-1,-1,-1):
-        cnt = 0
-        for i in range(p+1, len(prices)):
-            if prices[p] <= prices[i]:
-                cnt += 1
-            else:
-                cnt += 1
-                break
-        answer[p] = cnt
+    for i in range(len(prices)):
+        while stack != [] and stack[-1][1] > prices[i]:
+            past,_ = stack.pop()
+            answer[past] = i-past
+        stack.append([i, prices[i]])
+    
+    for i, s in stack:
+        answer[i] = len(prices)-1-i
     return answer
