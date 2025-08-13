a35fc82e5bc20af33b2fef13e244acf612a02f04
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-08-13 11:44:06 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4632 ms, Memory: 103556 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+def merge(left, right):
+    i, j = 0,0
+    sorted_list = []
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+
+    while i < len(left):
+        sorted_list.append(left[i])
+        i += 1
+    while j < len(right):
+        sorted_list.append(right[j])
+        j += 1
+
+    return sorted_list
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    leftarr = merge_sort(left)
+    rightarr = merge_sort(right)
+    return merge(leftarr,rightarr)
+
+
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+a_list = merge_sort(arr)
+for a in a_list:
+    print(a)
