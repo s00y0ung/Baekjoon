33b45d8deecc1607f76adcf347c56f120029a0aa
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-08-13 11:56:33 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1020 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,42 +1,9 @@
 import sys
-input = sys.stdin.readline
-print = sys.stdout.write
 
-def merge(left, right):
-    i, j = 0,0
-    sorted_list = []
+n = int(input())
+li = list(map(int, sys.stdin.read().split()))
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            sorted_list.append(left[i])
-            i += 1
-        else:
-            sorted_list.append(right[j])
-            j += 1
+li.sort()
 
-    while i < len(left):
-        sorted_list.append(left[i])
-        i += 1
-    while j < len(right):
-        sorted_list.append(right[j])
-        j += 1
-
-    return sorted_list
-
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr)//2
-    left = arr[:mid]
-    right = arr[mid:]
-
-    leftarr = merge_sort(left)
-    rightarr = merge_sort(right)
-    return merge(leftarr,rightarr)
-
-
-N = int(input())
-arr = [int(input()) for _ in range(N)]
-a_list = merge_sort(arr)
-print("\n".join(map(str,a_list)))
+for x in li:
+    sys.stdout.write(str(x) + '\n')
