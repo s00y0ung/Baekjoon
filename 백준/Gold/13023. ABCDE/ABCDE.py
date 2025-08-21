15bd864d5947055ed460049223e13af48905e1d3
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-08-21 16:58:29 +0900

    [Gold V] Title: ABCDE, Time: 652 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,34 +1,31 @@
 import sys
-input = sys.stdin.readline
 
-def dfs(n, depth):
-    global flag
-    if depth >= 5 or flag:
-        flag = 1
-        return
+def backTracking(i, node, visited, depth):
+    if depth == 5:
+        return 1
 
-    visited[n] = 1
-    for k in graph[n]:
+    for k in node[i]:
         if visited[k] == 0:
-            dfs(k, depth+1)
+            visited[k] = 1
+            if backTracking(k, node, visited, depth+1):
+                return 1
+            visited[k] = 0
+    return 0
 
-    visited[n] = 0
+N,M = map(int, input().split())
+node = [[] for _ in range(N)]
+for k in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    node[a].append(b)
+    node[b].append(a)
 
-if __name__ == "__main__":
-    N, M = map(int, input().split())
-    graph = [[] for i in range(N)]
-    visited = [0 for i in range(N)]
-
-    for i in range(M):
-        f1,f2 = map(int, input().split())
-        graph[f1].append(f2)
-        graph[f2].append(f1)
-
-    flag = 0
-    for i in range(N):
-        dfs(i, 1)
-        if flag == 1:
-            break;
-
-    print(flag)
+visited = [0 for i in range(N)]
+for i in range(N):
+    visited[i] = 1
+    if backTracking(i, node, visited, 1):
+        print(1)
+        break
+    visited[i] = 0
+    if i == N-1:
+        print(0)
 
