c0df2dacd975ff2484f516345c4040ad1b9deb81
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-11-19 18:49:12 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 7072 ms, Memory: 952636 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,17 +1,21 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
-N, L = map(int, input().split())
-d = list(map(int, input().split()))
+def main():
+    N, L = map(int, input().split())
+    que = deque()
+    num = list(map(int, input().split()))
+    ans = []
+    for i in range(N):
+        while que and que[-1][1] > num[i]:
+            que.pop()
+        que.append([i, num[i]])
+        if i - que[0][0] >= L:
+            que.popleft()
+        ans.append(que[0][1])
+        
+    print(*ans)
 
-que = deque()
-ans = []
-for c in range(N):
-    while que and que[-1][1] > d[c]:
-        que.pop()
-    que.append([c,d[c]])
-    if que[-1][0] - que[0][0] >= L:
-        que.popleft()
-    ans.append(que[0][1])
-print(*ans)
+if __name__ == '__main__':
+    main()
