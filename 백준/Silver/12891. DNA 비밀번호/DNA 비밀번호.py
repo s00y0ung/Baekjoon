9948dc13c9d242c51a58d9016af84fb7cc34140e
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-11-19 15:22:18 +0900

    [Silver II] Title: DNA 비밀번호, Time: 248 ms, Memory: 34344 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,18 +1,22 @@
-s, p = map(int, input().split())
-dna = input()
-a,c,g,t = map(int, input().split())
-
-dna_dic = {'A':0, 'C':0, 'G':0, 'T':0}
-ans = 0
-
-for i in range(p):
-    dna_dic[dna[i]] += 1
-if dna_dic['A'] >= a and dna_dic['C'] >= c and dna_dic['G'] >= g and dna_dic['T'] >= t:
-    ans += 1
-
-for i in range(p, s):
-    dna_dic[dna[i-p]] -= 1
-    dna_dic[dna[i]] += 1
+def main():
+    s, p = map(int, input().split())
+    dna = input()
+    a,c,g,t = map(int, input().split())
+    
+    dna_dic = {'A':0, 'C':0, 'G':0, 'T':0}
+    ans = 0
+    
+    for i in range(p):
+        dna_dic[dna[i]] += 1
     if dna_dic['A'] >= a and dna_dic['C'] >= c and dna_dic['G'] >= g and dna_dic['T'] >= t:
         ans += 1
-print(ans)
+    
+    for i in range(p, s):
+        dna_dic[dna[i-p]] -= 1
+        dna_dic[dna[i]] += 1
+        if dna_dic['A'] >= a and dna_dic['C'] >= c and dna_dic['G'] >= g and dna_dic['T'] >= t:
+            ans += 1
+    print(ans)
+
+if __name__ == '__main__':
+    main()
