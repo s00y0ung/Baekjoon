dcf30de718a2910ea74cd9d7da4f8980cdefcb06
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-11-24 22:14:59 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,3 +1,10 @@
-s = list(input())
-s.sort(reverse = True)
-print(''.join(s))
+import sys
+input = sys.stdin.readline
+
+def main():
+    arr = list(input().strip())
+    arr.sort(reverse = True)
+    print(''.join(arr))
+
+if __name__ == '__main__':
+    main()
