9a47ceb2b7a6d97b699e0f167e20b290727f9522
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-11-25 20:32:13 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 5044 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,16 +1,17 @@
 import sys
-input = sys.stdin.readline
 
 def main():
-    N = int(input())
-    arr = [0]*10001
-    for i in range(N):
-        a = int(input())
-        arr[a] += 1
-    
-    for i in range(10001):
-        for j in range(arr[i]):
-            print(i)
+    N = int(sys.stdin.readline())
+    arr = [0 for _ in range(10001)]
+    for _ in range(N):
+        arr[int(sys.stdin.readline())] += 1
+
+    for a in range(10001):
+        if arr[a] == 0:
+            continue
+        for j in range(arr[a]):
+            sys.stdout.write(str(a) + '\n')
+
 
 if __name__ == '__main__':
     main()
