48ef82b6110368e5058d01922397c22de973873a
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-11-27 12:44:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 40 ms, Memory: 33560 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,45 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-def bfs(s):
-    global visited, graph
-    visited[s] = 1
-    queue = [s]
+def main():
+    N, M, V = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    for i in range(M):
+        a,b = map(int, input().split())
+        graph[b].append(a)
+        graph[a].append(b)
 
-    while queue:
-        q = queue.pop(0)
-        print(q, end=" ")
+    for i in range(N+1):
+        graph[i].sort()
+    visited = [0 for _ in range(N+1)]
+    dfs(graph, V, visited)
+    print()
+    visited = [0 for _ in range(N + 1)]
+    bfs(graph, V, visited)
 
-        for e in graph[q]:
-            if not visited[e]:
-                visited[e] = 1
-                queue.append(e)
+def dfs(graph,V,visited):
+    print(V, end = ' ')
+    visited[V] = 1
+    for i in graph[V]:
+        if visited[i] == 0:
+            dfs(graph, i, visited)
 
+def bfs(graph, V, visited):
 
-def dfs(s):
-    global visited, graph
-    visited[s] = 1
-    print(s, end=" ")
+    que = [V]
+    visited[V] = 1
+    while que:
+        cur = que.pop(0)
+        print(cur, end = ' ')
 
-    for e in graph[s]:
-        if not visited[e]:
-            dfs(e)
+        for i in graph[cur]:
+            if visited[i] == 0:
+                visited[i] = 1
+                que.append(i)
 
 
 if __name__ == "__main__":
-    N, M, V = map(int, input().split())
-
-    graph = [[]for i in range(N+1)]
-    visited = [0 for i in range(N+1)]
-    for i in range(M):
-        s, e = map(int, input().split())
-        graph[s].append(e)
-        graph[e].append(s)
-
-    for i in range(1, N+1):
-        graph[i].sort()
-    dfs(V)
-    print()
-
-    visited = [0 for i in range(N+1)]
-    bfs(V)
+    main()
