b95c5a6306362c13d260061352bb39b932fa50c3
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-11-27 13:17:28 +0900

    [Silver I] Title: 미로 탐색, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,30 +1,23 @@
 import sys
-import copy
 input = sys.stdin.readline
 
-def bfs():
-    global N, M, map, visited
-    visited[0][0] = 1
-    queue = [[0,0]]
-
-    while queue:
-        y,x = queue.pop(0)
-        for d1,d2 in [[0,1],[0,-1],[1,0],[-1,0]]: #right, left, up, down
-            ey = y + d1
-            ex = x + d2
-            if 0 <= ex < M and 0 <= ey < N:
-                if visited[ey][ex] == 0 and map[ey][ex] == 1:
-                    visited[ey][ex] = visited[y][x] + 1
-                    queue.append([ey,ex])
-        if visited[N-1][M-1] != 0:
+def main():
+    N, M = map(int, input().split())
+    graph = []
+    for _ in range(N):
+        graph.append(list(input().strip()))
+    visited = [[0 for _ in range(M)] for _ in range(N)]
+    que = [(0,0,1)]
+    while que:
+        curx, cury, cnt = que.pop(0)
+        if curx == N-1 and cury == M-1:
+            print(cnt)
             break
-
-
+        for ex, ey in [(0,1), (0,-1), (-1,0), (1,0)]:
+            if 0 <= curx+ex < N and 0 <= cury+ey < M:
+                if graph[curx+ex][cury+ey]== '1' and visited[curx+ex][cury+ey] == 0:
+                    visited[curx+ex][cury+ey] = 1
+                    que.append((curx+ex, cury+ey, cnt+1))
 
 if __name__ == "__main__":
-    N, M = map(int, input().split())
-    map = [list(map(int, input().rstrip())) for i in range(N)]
-    visited = [[0 for i in range(M)] for i in range(N)]
-
-    bfs()
-    print(visited[N-1][M-1])
+    main()
