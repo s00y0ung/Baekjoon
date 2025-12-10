e571bf327448f83b290e279385efb95d29b7b029
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-12-10 20:57:03 +0900

    [Gold V] Title: 회의실 배정, Time: 248 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,21 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-if __name__ == "__main__":
+def main():
     N = int(input())
+    conference = []
+    for _ in range(N):
+        start, end = map(int, input().split())
+        conference.append((start, end))
+    conference.sort(key = lambda x : (x[1],x[0]))
 
-    meeting = []
-    for i in range(N):
-        s, e = map(int, input().split())
-        meeting.append([s,e])
-    meeting.sort(key = lambda x : (x[1],x[0]))
-
-    cnt = 0
-    end = -1
-    for i in range(N):
-        if meeting[i][0] >= end:
-            end = meeting[i][1]
+    cnt = 1
+    end_t = conference[0][1]
+    for i in range(1,N):
+        if end_t <= conference[i][0]:
             cnt += 1
-
+            end_t = conference[i][1]
     print(cnt)
-
+if __name__ == '__main__':
+    main()
