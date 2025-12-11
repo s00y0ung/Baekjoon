93fed7ed3f0ef6037ff3736c9d9adbe4a9e875bc
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-12-11 18:18:32 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 548 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -1,14 +1,18 @@
-def solve():
-    MIN, MAX = map(int, input().split())
-    prime = [ 1 for i in range(MIN, MAX+1)]
+import sys
+input = sys.stdin.readline
 
-    for i in range(2, int(MAX ** 0.5)+1):
-        s = i*i
-        s_idx = (MIN//s) * s
-        for j in range(s_idx, MAX+1, i*i):
+def main():
+    MIN, MAX = map(int, input().split())
+    prime = [1 for i in range(MIN, MAX + 1)]
+
+    for i in range(2, int(MAX ** 0.5) + 1):
+        s = i * i
+        s_idx = (MIN // s) * s
+        for j in range(s_idx, MAX + 1, i * i):
             if j >= MIN:
-                prime[j-MIN] = 0
+                prime[j - MIN] = 0
 
     print(sum(prime))
 
-solve()
+if __name__ == '__main__':
+    main()
