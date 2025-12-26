df1520b98267b965b47e7e2b1c85cad51256a124
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-12-26 15:09:16 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,43 +1,43 @@
-def union(x, y):
-    x_root = find(x)
-    y_root = find(y)
-    if x_root == y_root:
-        return
+import sys
+input = sys.stdin.readline
 
-    if parent[x_root] < parent[y_root]:
-        parent[x_root] += parent[y_root]
-        parent[y_root] = x_root
+def find(n,parent):
+    while n != parent[n]:
+        n = parent[n]
+    return n
+
+def union(a,b,size,parent):
+    roota = find(a, parent)
+    rootb = find(b, parent)
+
+    if roota == rootb: return
+
+    if size[roota] < size[rootb]:
+        parent[roota] = rootb
+        size[rootb] += size[roota]
     else:
-        parent[y_root] += parent[x_root]
-        parent[x_root] = y_root
+        parent[rootb] = roota
+        size[roota] += size[rootb]
 
-def find(x):
-    if parent[x] < 0:
-        return x
-    return find(parent[x])
+def main():
+    N = int(input())
+    M = int(input())
 
+    parent = [i for i in range(N+1)]
+    size = [1]*(N+1)
+    for i in range(1,N+1):
+        connect = list(map(int, input().split()))
+        for j in range(N):
+            if connect[j] == 1:
+                union(i,j+1,size,parent)
 
-N = int(input()) # N <= 200
-M = int(input()) # M <= 1000
+    plan = list(map(int, input().split()))
+    plan_root = find(plan[0], parent)
+    for p in plan[1:]:
+        if plan_root != find(p, parent):
+            print('NO')
+            return
+    print('YES')
 
-parent = [-1] * (N+1)
-graph =[[] for _ in range(N+1)]
-
-for i in range(1, 1+N):
-    graph[i] = list(map(int, input().split()))
-    graph[i].insert(0,0)
-plan = list(map(int, input().split()))
-
-for k in range(1,1+N):
-    for i in range(1,len(graph[k])):
-        if graph[k][i] == 1:
-            union(k, i)
-
-flag = 1
-pre = plan[0]
-for cur in plan[1:]:
-    if find(pre) != find(cur):
-        flag = 0
-        break
-if flag: print("YES")
-else: print("NO")
+if __name__ == "__main__":
+    main()
