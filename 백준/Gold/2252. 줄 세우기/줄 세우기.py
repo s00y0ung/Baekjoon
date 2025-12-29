12aae7667c3fa50122fcc99b38a17e7da557fc03
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2025-12-29 16:34:02 +0900

    [Gold III] Title: 줄 세우기, Time: 156 ms, Memory: 39600 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,33 +1,30 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def topo_sort():
+def main():
+    N,M = map(int, input().split())
+    income = [0]*(N+1)
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a,b = map(int, input().split())
+        graph[a].append(b)
+        income[b] += 1
 
-    while queue:
-        cur = queue.popleft()
-        result.append(cur)
+    que = []
+    for i in range(1, N+1):
+        if income[i] == 0:
+            que.append(i)
 
-        for idx in graph[cur]:
-            indegree[idx] -= 1
-            if indegree[idx] == 0:
-                queue.append(idx)
+    ans = []
+    while que:
+        cur = que.pop(0)
+        ans.append(cur)
+        for i in graph[cur]:
+            if income[i] != 0:
+                income[i] -= 1
+                if income[i] == 0:
+                    que.append(i)
+    print(*ans)
 
-N, M = map(int, input().split())
-
-graph = [[] for _ in range(N+1)]
-indegree = [0 for _ in range(N+1)]
-queue = deque()
-result = []
-
-for i in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    indegree[b] += 1
-
-for idx in range(1, len(indegree)):
-    if indegree[idx] == 0:
-        queue.append(idx)
-
-topo_sort()
-print(*result)
+if __name__ == "__main__":
+    main()
