a14148704917a4b38b6d9c199ee8b9332661b493
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-02-19 17:05:54 +0900

    [Gold III] Title: 나머지 합, Time: 384 ms, Memory: 157284 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
-nums = list(map(int, input().split()))
+def main():
+    N,M = map(int, input().split())
+    a_list = list(map(int,input().split()))
+    s_list = [0]*(N+1)
+    remain = [0]*(M)
+    for i in range(1,N+1):
+        s_list[i] = s_list[i-1]+a_list[i-1]
+        remain[s_list[i]%M] += 1
+    ans = remain[0]
+    for i in range(M):
+        tmp = remain[i]
+        ans += max(0, tmp*(tmp+1)//2-tmp)
+    print(ans)
 
-prefix = [0]
-div = [0 for i in range(m)]
-for i in range(n):
-    prefix.append(prefix[-1]+nums[i])
-    div[prefix[-1]%m] += 1
-
-ans = div[0]
-for i in range(m):
-    a = div[i]
-    ans += (a*(a-1)//2)
-print(ans)
+main()
