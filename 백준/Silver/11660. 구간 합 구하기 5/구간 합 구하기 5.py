fea5c43f492801d79c7b9773c7c99ab7541fe697
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-02-19 16:32:58 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 732 ms, Memory: 106756 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,31 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-arr = []
-prefix_sum = [0 for i in range(N+1)]
-s = 0
-
-for i in range(N):
-    arr.append(list(map(int, input().split())))
+def main():
+    N,M = map(int, input().split())
+    a_list = []
+    s_list = [[0]*(N+1) for _ in range(N+1)]
+    for i in range(N):
+        a_list.append(list(map(int, input().split())))
+    for x in range(1,N+1):
+        for y in range(1, N+1):
+            s_list[x][y] = s_list[x-1][y]+s_list[x][y-1]-s_list[x-1][y-1]+a_list[x-1][y-1]
     
-    prefix = 0
-    prefix_sum.append(0)
-    for j in range(N):
-        if i == 0:
-            s = s + arr[i][j]
-            prefix_sum.append(s)
-            continue
-            
-        prefix = prefix + arr[i][j]
-        prefix_sum.append(prefix + prefix_sum[(i)*(1+N)+(j+1)])
-
-for i in range(M):
-    x1, y1, x2, y2 = map(int, input().split())
-
-    tmp1 = prefix_sum[(N+1)*(x1-1) + (y1-1)]
-    tmp2 = prefix_sum[(N+1)*(x1-1) + (y2)]
-    tmp3 = prefix_sum[(N+1)*(x2) + (y1-1)]
-    tmp4 = prefix_sum[(N+1)*(x2) + (y2)]
-    
-    print(tmp4 - tmp2 - tmp3 + tmp1)
+    for _ in range(M):
+        xs,ys,xe,ye = map(int, input().split())
+        print(s_list[xe][ye]-s_list[xe][ys-1]-s_list[xs-1][ye]+s_list[xs-1][ys-1])
+main()
