edaf99697f83ef412bbc0307dffa433838b2fbbd
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-02-23 14:12:08 +0900

    [Gold I] Title: 최솟값 찾기, Time: 5688 ms, Memory: 871808 KB -BaekjoonHub

---
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def main():
+    N ,L = map(int, input().split())
+    num = list(map(int, input().split()))
+
+    d = deque()
+    ans = []
+    for i in range(N):
+        while d and i-d[0][1] >= L:
+            d.popleft()
+        while d and d[len(d)-1][0] > num[i]:
+            d.pop()
+        d.append((num[i],i))
+        ans.append(d[0][0])
+    print(*ans)
+if __name__ == '__main__':
+    main()
