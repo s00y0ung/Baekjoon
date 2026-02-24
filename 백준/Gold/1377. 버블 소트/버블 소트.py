069736332739b399dcd20d2fb8918afd6a126b05
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-02-24 12:26:23 +0900

    [Gold II] Title: 버블 소트, Time: 660 ms, Memory: 109740 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.py
+++ 백준/Gold/1377. 버블 소트/버블 소트.py
@@ -3,14 +3,17 @@ input = sys.stdin.readline
 
 def main():
     N = int(input())
-    arr = [0] * N
+    arr = []
     for i in range(N):
-        arr[i] = [int(input()),i]
-    arr = sorted(arr, key = lambda x:x[0])
+        arr.append((int(input()),i))
+    arr.sort(key = lambda x:x[0])
+
     ans = 0
     for i in range(N):
-        if ans < (arr[i][1]-i+1):
-            ans = (arr[i][1]-i+1)
-    print(ans)
+        if ans < arr[i][1]-i:
+            ans = arr[i][1]-i
+
+    print(ans+1)
+
 if __name__ == '__main__':
     main()
