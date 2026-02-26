0e17de057f5212fe72e15611c549564ba51e1f5e
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-02-26 16:55:18 +0900

    [Gold I] Title: K번째 수, Time: 360 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -3,23 +3,23 @@ input = sys.stdin.readline
 
 def main():
     N = int(input())
-    K = int(input())
+    M = int(input())
 
-    start, end = 1, K
-    answer = 0
+    start = 1
+    end = M
+    ans = 0
+
     while start <= end:
-        num = 0
-        mid = (start+end) // 2
-
+        mid = (start+end)//2
+        cnt = 0
         for i in range(1,N+1):
-            num += min(mid//i, N)
-
-        if num >= K:
+            cnt += min(mid//i, N)
+        if cnt >= M:
+            ans = mid
             end = mid-1
-            answer = mid
         else:
             start = mid+1
-    print(answer)
+    print(ans)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
