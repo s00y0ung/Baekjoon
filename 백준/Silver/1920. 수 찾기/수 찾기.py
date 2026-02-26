20114d0db0945091a766c86d6314caaef73f4974
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-02-26 14:09:01 +0900

    [Silver IV] Title: 수 찾기, Time: 416 ms, Memory: 49160 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,29 +1,28 @@
-def binary_search(search):
-    global N_list, N
+import sys
+input = sys.stdin.readline
 
-    left = 0
-    right = N-1
+def binary_search(check, arr):
+    low, high = 0, len(arr)-1
 
-    while left <= right:
-        mid = (left + right)//2
-        if N_list[mid] > search:
-            right = mid-1
-        elif N_list[mid] < search:
-            left = mid+1
-        else:
+    while low <= high:
+        mid = (low+high) // 2
+        if arr[mid] == check:
             return 1
-
+        elif arr[mid] < check:
+            low = mid+1
+        else:
+            high = mid-1
     return 0
 
+def main():
+    N = int(input())
+    arr = list(map(int, input().split()))
+    M = int(input())
+    check = list(map(int, input().split()))
 
+    arr.sort()
+    for i in range(M):
+        print(binary_search(check[i],arr))
 
 if __name__ == "__main__":
-    N = int(input())
-    N_list = list(map(int, input().split()))
-    N_list.sort()
-
-    M = int(input())
-    M_list = list(map(int, input().split()))
-
-    for m in M_list:
-        print(binary_search(m))
+    main()
