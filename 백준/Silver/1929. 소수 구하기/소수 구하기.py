d81565208e9d952d248d6bb050a15d06a4439fe2
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-04 10:28:19 +0900

    [Silver III] Title: 소수 구하기, Time: 172 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 def main():
     M, N = map(int, input().split())
 
-    prime = [1] * (N+1)
-    prime[0] = 0
+    prime = [1]*(N+1)
     prime[1] = 0
-    for i in range(2, N+1):
+    for i in range(2, int(N**0.5)+1):
         if prime[i] == 1:
             for j in range(i+i,N+1,i):
                 prime[j] = 0
 
-    for i in range(M,N+1):
-        if prime[i] == 1:
+    for i in range(M, N+1):
+        if prime[i]:
             print(i)
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
