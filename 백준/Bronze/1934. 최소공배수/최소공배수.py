624964b7406c1349604e1e607c32729a4f2fa9ab
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-11 14:34:20 +0900

    [Bronze I] Title: 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
-    A, B = map(int, input().split())
-    m,n = A,B
-    while n != 0:
-        m,n = n, m%n
-    # 최대 공약수 == m
-    print(A*B//m)
-
-T = int(input())
-for i in range(T):
-    solve()
+def main():
+    T = int(input())
+    for _ in range(T):
+        a,b = map(int, input().split())
+        tmp = a*b
+        while b > 0:
+            a,b = b,a%b
+        print(tmp//a)
+        
+if __name__ == '__main__':
+    main()
