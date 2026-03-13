b636b0074801dc428678205f4bc5bd3298d0f7bd
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-13 11:26:17 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,43 +1,40 @@
 import sys
 input = sys.stdin.readline
 
-def find(n,parent):
-    while n != parent[n]:
-        n = parent[n]
-    return n
+def find(x, parent):
+    while x != parent[x]:
+        x = parent[x]
+    return x
 
-def union(a,b,size,parent):
-    roota = find(a, parent)
-    rootb = find(b, parent)
+def union(a,b,parent):
+    ra = find(a,parent)
+    rb = find(b,parent)
 
-    if roota == rootb: return
-
-    if size[roota] < size[rootb]:
-        parent[roota] = rootb
-        size[rootb] += size[roota]
+    if ra == rb: return
+    elif ra > rb:
+        parent[rb] = ra
     else:
-        parent[rootb] = roota
-        size[roota] += size[rootb]
+        parent[ra] = rb
+
 
 def main():
-    N = int(input())
-    M = int(input())
+    n = int(input())
+    m = int(input())
+    parent = [i for i in range(n+1)]
 
-    parent = [i for i in range(N+1)]
-    size = [1]*(N+1)
-    for i in range(1,N+1):
-        connect = list(map(int, input().split()))
-        for j in range(N):
-            if connect[j] == 1:
-                union(i,j+1,size,parent)
+    for i in range(1,n+1):
+        tmp = list(map(int, input().split()))
+        for t in range(n):
+            if tmp[t] == 1:
+                union(i,t+1,parent)
 
     plan = list(map(int, input().split()))
-    plan_root = find(plan[0], parent)
-    for p in plan[1:]:
-        if plan_root != find(p, parent):
+    rp = find(plan[0], parent)
+    for p in plan:
+        if rp != find(p,parent):
             print('NO')
             return
     print('YES')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
