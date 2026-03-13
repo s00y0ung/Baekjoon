520bedfe333200d212e5cedf9e349847e5fd1988
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-13 12:39:02 +0900

    [Gold III] Title: 줄 세우기, Time: 164 ms, Memory: 43696 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -2,29 +2,28 @@ import sys
 input = sys.stdin.readline
 
 def main():
-    N,M = map(int, input().split())
-    income = [0]*(N+1)
-    graph = [[] for _ in range(N+1)]
-    for _ in range(M):
+    n,m = map(int, input().split())
+    income = [[0,i] for i in range(n+1)]
+    g = [[] for _ in range(n+1)]
+
+    for _ in range(m):
         a,b = map(int, input().split())
-        graph[a].append(b)
-        income[b] += 1
+        g[a].append(b)
+        income[b][0] += 1
 
     que = []
-    for i in range(1, N+1):
-        if income[i] == 0:
+    for i in range(1,n+1):
+        if income[i][0] == 0:
             que.append(i)
-
     ans = []
     while que:
         cur = que.pop(0)
         ans.append(cur)
-        for i in graph[cur]:
-            if income[i] != 0:
-                income[i] -= 1
-                if income[i] == 0:
-                    que.append(i)
+        for i in g[cur]:
+            income[i][0] -= 1
+            if income[i][0] == 0:
+                que.append(i)
     print(*ans)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
