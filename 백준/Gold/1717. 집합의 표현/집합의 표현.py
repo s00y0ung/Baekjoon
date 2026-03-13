bb8d92099d0c98210c3a143a64d10ed9420b810e
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-13 11:01:35 +0900

    [Gold V] Title: 집합의 표현, Time: 272 ms, Memory: 80656 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,37 +1,37 @@
 import sys
 input = sys.stdin.readline
 
-# union by rank
-#음수일 때 루트 크기 저장, 양수일 땐, 부모 인덱스 저장하는 방식임 ...
-def union(b,c):
-    b_root = find(b)
-    c_root = find(c)
+def find(parent,x):
+    while x != parent[x]:
+        x = parent[x]
+    return x
 
-    if b_root == c_root: #이미 같은 집합인 경우
+def union(a,b,parent,size):
+    root_a = find(parent,a)
+    root_b = find(parent,b)
+
+    if root_a == root_b:
         return
-
-    if parent[b_root] < parent[c_root]: #음수 비교, b의 집합 크기가 더 큰 경우
-        parent[b_root] += parent[c_root]
-        parent[c_root] = b_root
+    elif size[root_a] > size[root_b]:
+        parent[root_b] = root_a
+        size[root_a] += size[root_b]
     else:
-        parent[c_root] += parent[b_root]
-        parent[b_root] = c_root
+        parent[root_a] = root_b
+        size[root_b] += size[root_a]
 
+def main():
+    n,m = map(int, input().split())
+    parent = [i for i in range(n+1)]
+    size = [1 for _ in range(n+1)]
+    for _ in range(m):
+        z, a,b = map(int, input().split())
+        if z == 0:
+            union(a,b,parent,size)
+        elif z == 1:
+            if find(parent,a) == find(parent,b):
+                print('yes')
+            else:
+                print('no')
 
-def find(x):
-    if parent[x] < 0:
-        return x
-    return find(parent[x])
-
-n, m = map(int, input().split())
-parent = [-1 for i in range(n+1)]
-
-for i in range(m):
-    a,b,c  = map(int, input().split())
-    if a == 0:
-        union(b,c)
-    elif a == 1:
-        if find(b) == find(c):
-            print("YES")
-        else:
-            print("NO")
+if __name__ == '__main__':
+    main()
