90613c44a61de1994f7e158925e9cadc546ccebd
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-18 17:25:37 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,34 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-def preorder(root):
-    print(chr(root + 65), end ="")
-    if tree[root][0] != -19:
-        preorder(tree[root][0])
-    if tree[root][1] != -19:
-        preorder(tree[root][1])
+def preorder(tree,s):
+    if s == -1:
+        return
+    print(chr(s+65), end='')
+    preorder(tree,tree[s][0])
+    preorder(tree,tree[s][1])
 
-def inorder(root):
-    if tree[root][0] != -19:
-        inorder(tree[root][0])
-    print(chr(root+65), end = "")
-    if tree[root][1] != -19:
-        inorder(tree[root][1])
+def inorder(tree,s):
+    if s == -1:
+        return
+    inorder(tree,tree[s][0])
+    print(chr(s+65), end='')
+    inorder(tree,tree[s][1])
 
-def postorder(root):
-    if tree[root][0] != -19:
-        postorder(tree[root][0])
-    if tree[root][1] != -19:
-        postorder(tree[root][1])
-    print(chr(root + 65), end ="")
+def postorder(tree,s):
+    if s == -1:
+        return
+    postorder(tree,tree[s][0])
+    postorder(tree,tree[s][1])
+    print(chr(s+65), end='')
 
-N = int(input())
-tree = [[] for _ in range(26)]
-for _ in range(N):
-    root, left, right = input().split()
-    tree[ord(root)-65] = [ord(left)-65, ord(right)-65]
-preorder(0)
-print()
-inorder(0)
-print()
-postorder(0)
+def main():
+    n = int(input())
+    tree = [[] for _ in range(n)]
+    for _ in range(n):
+        a,b,c = map(str,input().split())
+        tree[ord(a)-65] = [max(-1,ord(b)-65), max(-1,ord(c)-65)]
+
+    preorder(tree,0)
+    print()
+    inorder(tree,0)
+    print()
+    postorder(tree,0)
+
+if __name__ == '__main__':
+    main()
