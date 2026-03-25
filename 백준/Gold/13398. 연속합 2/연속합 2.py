c8f979189ac4ca9ec367c76c0f2130ace7e1fdd3
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-25 12:03:45 +0900

    [Gold IV] Title: 연속합 2, Time: 108 ms, Memory: 45528 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,23 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-n_list = list(map(int, input().split()))
-prefix_l = [0] * (N)
-prefix_r = [0] * (N)
-max_n = n_list[0]
+def main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    L = [0 for _ in range(n)]
+    R = [0 for _ in range(n)]
+    ans = arr[0]
+    
+    L[0] = arr[0]
+    for i in range(1,n):
+        L[i] = max(L[i-1] + arr[i], arr[i])
+        ans = max(ans, L[i])
+    R[-1] = arr[-1]
+    for i in range(n-2,-1,-1):
+        R[i] = max(R[i+1]+arr[i], arr[i])
+    
+    for i in range(1,n-1):
+        ans = max(L[i-1]+R[i+1],ans)
+    print(ans)
 
-prefix_l[0] = n_list[0]
-for i in range(1, N):
-    prefix_l[i] = max(prefix_l[i-1]+n_list[i], n_list[i])
-    if max_n < prefix_l[i]:
-        max_n = prefix_l[i]
-
-prefix_r[N-1] = n_list[N-1]
-for j in range(N-2,-1,-1):
-    prefix_r[j] = max(prefix_r[j+1]+n_list[j], n_list[j])
-
-for idx in range(1,N-1):
-    if max_n < prefix_l[idx-1] + prefix_r[idx+1]:
-        max_n = prefix_l[idx-1] + prefix_r[idx+1]
-print(max_n)
+if __name__ == '__main__':
+    main()
