234e95fad13124531dd7aef39a623b9ae2378d7f
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-25 12:48:49 +0900

    [Gold III] Title: LCS 2, Time: 560 ms, Memory: 420884 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -5,25 +5,20 @@ def main():
     s1 = list(input().strip())
     s2 = list(input().strip())
 
-    d = [[[0,''] for _ in range(len(s1)+1)] for _ in range(len(s2)+1)]
-    ans = [0,'']
+    d = [['' for _ in range(len(s1)+1)] for _ in range(len(s2)+1)]
     for i in range(1,len(s2)+1):
         for j in range(1,len(s1)+1):
             if s2[i-1] == s1[j-1]:
-                d[i][j][0] = d[i-1][j-1][0]+1
-                d[i][j][1] = d[i-1][j-1][1]+s2[i-1]
+                d[i][j] = d[i-1][j-1]+s2[i-1]
             else:
-                if d[i-1][j][0] > d[i][j-1][0]:
-                    d[i][j][0] = d[i-1][j][0]
-                    d[i][j][1] = d[i-1][j][1]
+                if len(d[i-1][j]) > len(d[i][j-1]):
+                    d[i][j] = d[i-1][j]
                 else:
-                    d[i][j][0] = d[i][j-1][0]
-                    d[i][j][1] = d[i][j-1][1]
-            if d[i][j][0] > ans[0]:
-                ans = [d[i][j][0], d[i][j][1]]
-    print(ans[0])
-    if ans[0] != 0:
-        print(ans[1])
+                    d[i][j] = d[i][j-1]
+
+    print(len(d[-1][-1]))
+    if len(d[-1][-1]) != 0:
+        print(d[-1][-1])
 
 if __name__ == '__main__':
     main()
