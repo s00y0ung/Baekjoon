7c2fdd0d1b397e28ab6dbb81d590f5fcfe77e749
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-26 22:42:09 +0900

    [Gold I] Title: 외판원 순회, Time: 988 ms, Memory: 49432 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -1,32 +1,31 @@
 import sys
+input = sys.stdin.readline
 
-INF = 1e+9
-n = int(input())
-mat = []
-for i in range(n):
-    mat.append(list(map(int, sys.stdin.readline().split())))
-    
-dp = [[None for _ in range(1<<n)] for _ in range(n)]    #모든 방문 가능 경우의 수
-chk = 1
-
-def search(node, chk):
-    if chk==(1<<n)-1:
-        if mat[node][0]!=0:
+def search(n,mat,dp,node, chk):
+    if chk == (1<<n)-1:
+        if mat[node][0] != 0:
             return mat[node][0]
         else:
-            return INF
-
-    if dp[node][chk]!=None: 
+            return 1e9
+    if dp[node][chk] != None:
         return dp[node][chk]
 
-    min_value = INF
+    min_value = 1e9
     for i in range(1, n):
-        if (mat[node][i]!= 0) and ((chk & (1<<i))==0):    #연결되어 있으며 방문한적 없을때
-            min_value = min(min_value, search(i, chk | (1<<i)) + mat[node][i])
+        if (mat[node][i] != 0) and ((chk & (1 << i)) == 0):
+            min_value = min(min_value, search(n,mat,dp,i, chk | (1 << i)) + mat[node][i])
     dp[node][chk] = min_value
 
     return min_value
 
+def main():
+    n = int(input())
+    mat = []
+    for _ in range(n):
+        mat.append(list(map(int, input().split())))
+    dp = [[None for _ in range(1 << n)] for _ in range(n)]  # 모든 방문 가능 경우의 수
 
-answer = search(0,chk)
-print(answer)
+    chk = 1
+    print(search(n,mat,dp,0,chk))
+if __name__ == '__main__':
+    main()
