10be2e6ee5f52e50483af614d85aa38b407d7ea2
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2026-03-27 11:39:11 +0900

    [Gold V] Title: CCW, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.py
+++ 백준/Gold/11758. CCW/CCW.py
@@ -1,11 +1,18 @@
-x1, y1 = map(int, input().split())
-x2, y2 = map(int, input().split())
-x3, y3 = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-ccw = (x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3)
-if ccw < 0:
-    print(-1)
-elif ccw == 0:
-    print(0)
-else:
-    print(1)
+def main():
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+    x3, y3 = map(int, input().split())
+
+    ccw = (x1*y2+x2*y3+x3*y1) - (x2*y1+x3*y2+x1*y3)
+    if ccw > 0:
+        print(1)
+    elif ccw == 0:
+        print(0)
+    else:
+        print(-1)
+
+if __name__ == '__main__':
+    main()
